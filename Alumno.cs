aff4d3c557209d7b6a2b42ae5dfe85b911cdae49
Claudia Cruz <claudiacruz85@gmail.com>
2015-03-18 10:46:31 -0600

    Practica3
    Agenda de Alumnos

---
+++ Alumno.cs
@@ -0,0 +1,48 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Claudia
+ * Date: 11/03/2015
+ * Time: 10:34 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+
+using System;
+using System.Collections;
+
+namespace Practica_3
+{
+
+	public class Alumno
+	{
+		private String nombre;
+		private String codigo;
+		
+		public Alumno(String n, String c)
+		{
+			nombre=n;
+			codigo=c;
+		}
+		public Alumno(){
+			nombre="";
+			codigo="";
+		}
+		
+		public void setNombre(String nombre){
+			this.nombre=nombre;
+		}
+		public String getNombre(){
+			return nombre;
+		}
+		public void setCodigo(String nombre){
+			this.nombre=nombre;
+	}
+		public String getCodigo(){
+			return codigo;
+		}
+		
+		public String getDatos(){
+			return "nombre: "+nombre+"\n" + "Codigo: "+codigo+"\n*********************";
+		}
+	}
+}
---
+++ Main.cs
@@ -0,0 +1,80 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Claudia
+ * Date: 11/03/2015
+ * Time: 10:25 a. m.
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+using System.Collections;
+ 
+ namespace Practica_3
+ {
+ 	class Program
+ 	{
+ 		private Hashtable tabla;
+ 		
+ 		public Program(){
+ 			tabla=new Hashtable();	
+ 		}
+ 		public void agregarAlumno(){
+ 			String nombre;
+ 			String codigo;
+ 			Console.Write("Nombre del alumno: ");
+ 			nombre=Console.ReadLine();
+ 			Console.Write("Codigo del alumno: ");
+ 			codigo=Console.ReadLine();
+ 			Alumno alumno=new Alumno(nombre,codigo);
+ 			tabla.Add(codigo,alumno);
+ 		}
+ 		public void verAlumnos(){
+ 			Console.WriteLine("\n****** Alumnos Registrados *******\n");
+ 			foreach ( string codigo in tabla.Keys){
+ 				try{
+ 					Object obj= tabla[codigo];
+ 					Alumno alumno= (Alumno) obj;
+ 					Console.WriteLine(alumno.getDatos());
+ 					
+ 				}catch(Exception e){
+ 					Console.WriteLine("Error");
+ 				}
+ 			}
+ 			
+ 		}
+ 		
+ 		public void eliminarAlumno(){
+ 			String codigo;
+ 			Console.Write("Codigo del alumno: ");
+ 			codigo=Console.ReadLine();
+ 			if(tabla.ContainsKey(codigo)){
+ 			   	tabla.Remove(codigo);
+ 			   	Console.WriteLine("Procesando........ \nBorrado el alumno: "+ codigo);
+ 			}
+ 			   	else Console.WriteLine("No se encontro ese codigo "+codigo);
+ 			   	Console.WriteLine("\n*****************************");
+ 		}
+ 		
+ 		public static void Main(string[] args)
+ 		{
+ 			int opcion;
+ 			Program programa=new Program();
+ 			do{
+ 				Console.WriteLine("\n***** Agenda de Alumnos *****\n");
+ 				Console.WriteLine(" 1)Agregar Alumno\n 2)Mostrar Alumnos \n 3)Eliminar Alumno\n 4)Salir");
+ 				opcion=int.Parse(Console.ReadLine());
+ 				switch(opcion){
+ 						case 1:programa.agregarAlumno();
+ 						break;
+ 						case 2:programa.verAlumnos();
+ 						break;
+ 						case 3:programa.eliminarAlumno();
+ 						break;
+ 						default:Console.WriteLine("Estas saliendo del programa.....");
+ 						Console.ReadLine();
+ 						break;
+ 				}
+ 			}while (opcion<4);
+ 		}
+ 	}
+ }
